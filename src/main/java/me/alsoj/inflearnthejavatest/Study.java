9df10b53f9b5d4ec40a3765731abd7a2f6e7b200
alsoj <alsoj@naver.com>
2022-09-22 20:12:04 +0900

    modify test case

--- src/main/java/me/alsoj/inflearnthejavatest/Study.java
+++ src/main/java/me/alsoj/inflearnthejavatest/Study.java
@@ -5,11 +5,14 @@ public class Study {
     private StudyStatus status = StudyStatus.DRAFT;
     private int limit;
 
-    public Study(int limit) {
+    private String name;
+
+    public Study(int limit, String name) {
         if(limit < 0) {
             throw new IllegalArgumentException("limit은 0보다 커야 합니다.");
         }
         this.limit = limit;
+        this.name = name;
     }
 
     public StudyStatus getStatus() {
@@ -18,5 +21,18 @@ public class Study {
 
     public int getLimit() {
         return limit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/test/java/me/alsoj/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/alsoj/inflearnthejavatest/StudyTest.java
@@ -1,28 +1,62 @@
 package me.alsoj.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
+    @FastTest
     @Test
-    @DisplayName("스터디 만들기")
+    @DisplayName("스터디 만들기 fast")
     void create_new_study() {
         assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
             new Study(10);
-            Thread.sleep(300);
+            Thread.sleep(10);
         });
     }
 
+    @SlowTest
     @Test
-    @DisplayName("스터디 한 번 더 만들기")
+    @DisplayName("스터디 한 번 더 만들기 slow")
     void create_new_study_again() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study((-10)));
         assertEquals("limit은 0보다 커야 합니다.", exception.getMessage());
+    }
+
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value=10, name= "{displayName}, {currentRepetition}/{totalRepetition}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기 파라미터")
+    @ParameterizedTest(name="{index} {displayName}, message={0}")
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study){
+        System.out.println(study);
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
     }
 
     @BeforeAll
