67514976bebaf7927b20d09ff0782e92947d17a8
alsoj <alsoj@naver.com>
2022-09-27 21:32:03 +0900

    modify test case

--- src/test/java/me/alsoj/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/alsoj/inflearnthejavatest/StudyTest.java
@@ -17,24 +17,26 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StudyTest {
 
+    int value = 1;
+
+    @Order(2)
     @FastTest
     @Test
     @DisplayName("스터디 만들기 fast")
     void create_new_study() {
-        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(10);
-        });
+        System.out.println("value = " + value++);
     }
 
+    @Order(1)
     @SlowTest
     @Test
     @DisplayName("스터디 한 번 더 만들기 slow")
     void create_new_study_again() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study((-10)));
-        assertEquals("limit은 0보다 커야 합니다.", exception.getMessage());
+        System.out.println("value = " + value++);
     }
 
     @DisplayName("스터디 만들기")
@@ -46,18 +48,10 @@ class StudyTest {
     @DisplayName("스터디 만들기 파라미터")
     @ParameterizedTest(name="{index} {displayName}, message={0}")
     @ValueSource(ints = {10, 20, 40})
-    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study){
+    void parameterizedTest(Study study){
         System.out.println(study);
     }
 
-    static class StudyConverter extends SimpleArgumentConverter {
-
-        @Override
-        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
-            assertEquals(Study.class, targetType, "Can only convert to Study");
-            return new Study(Integer.parseInt(source.toString()));
-        }
-    }
 
     @BeforeAll
     static void beforeAll() {
